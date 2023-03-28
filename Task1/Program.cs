2970f58861870a8eee4121afd91c28845b6a9e86
Maxim <magmax@post.com>
2023-03-28 19:07:28 +0300

    init commit

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+//которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.WriteLine("input massive lenght: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int count = 0;
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[] array)
+{
+    for(int k = 0; k < size; k++)
+        Console.Write($"{array[k]}  ");
+}
+
+for(int j = 0; j < size; j++)
+{
+    if(array[j] % 2 == 00)
+    count++;
+}
+Console.WriteLine();
+Console.WriteLine($"the number of even numbers in the array = {count}");
---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+Console.WriteLine("input massive lenght: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+
+FillArray(array);
+PrintArray(array);
+double max = array[0];
+double min = array[0];
+
+void FillArray(double[] array)
+{
+    for(int i = 0; i < size; i++)
+    {
+        double a = new Random().Next(-999, 1000);
+        double b = new Random().Next(-99, 100);
+        double c = Math.Round(a / b, 2, MidpointRounding.AwayFromZero);
+        //Console.WriteLine($"{a}, {b}, {c}");
+        array[i] = c;
+    }
+}
+void PrintArray(double[] array)
+{
+    for(int k = 0; k < size; k++)
+        Console.Write($"{array[k]}  ");
+}
+
+for(int j = 1; j < size; j++)
+{
+    if(array[j] > max)
+        max = array[j];
+    if(array[j] < min)
+        min = array[j];
+}
+double diff = Math.Round(max - min, 2, MidpointRounding.AwayFromZero); ;
+Console.WriteLine();
+Console.WriteLine($"the difference between the maximum and minimum array elements = {diff}");
